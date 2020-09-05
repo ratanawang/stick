f8d6c5dff742b040dca7ca1f5d02dbade9fdda82
Ratana <ratanawang8@gmail.com>
2020-09-05 03:07:39 -0700

    Set up index.html, reorganize extensions directory

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,4 +28,8 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    from . import dashboard
+    app.register_blueprint(dashboard.bp)
+    app.add_url_rule("/", endpoint="index")
+
     return app
---
+++ flaskr/dashboard.py
@@ -0,0 +1,10 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+
+bp = Blueprint("dashboard", __name__)
+
+
+@bp.route("/")
+def index():
+    return render_template("index.html")
