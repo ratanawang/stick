9ca4c2343241db542b2987df762b1be09fa417a0
GcWan <GcWan@users.noreply.github.com>
2020-09-05 17:07:47 -0700

    Add enter command for adding notes

--- extension/static/js/popup.js
+++ extension/static/js/popup.js
@@ -1,29 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.addNewNote').addEventListener('click', onclick, false);
-  function onclick() {
-      let newNoteText = document.getElementById("newnote").value
-      if(newNoteText!=null && newNoteText!=""){
-          alert(newNoteText)
-          chrome.storage.sync.get(['notes'], function (result) {
-              if(result.notes != null){
-                  updatedNotes = result.notes;
-              }
-              else{
-                  updatedNotes = [];
-              }
-              updatedNotes.push(newNoteText);
-              chrome.storage.sync.set({'notes': updatedNotes}, function () {
-                  alert("New note added!");
-              })
-          })
-      }
 
-  }
 }, false)
 
 document.addEventListener('input', function (event) {
 	if (event.target.tagName.toLowerCase() !== 'textarea') return;
 	autoExpand(event.target);
+}, false);
+
+document.addEventListener('keypress', function (event) {
+	if (event.keyCode == 13) {
+	    let newNoteText = document.getElementById("newnote").value
+        if (newNoteText != null && newNoteText.trim() != "") {
+            newNoteText = newNoteText.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+                .replace(/\r?\n|\r/g, " ").trim()
+            alert(newNoteText)
+            chrome.storage.sync.get(['notes'], function (result) {
+                if (result.notes != null) {
+                    updatedNotes = result.notes;
+                } else {
+                    updatedNotes = [];
+                }
+                updatedNotes.push(newNoteText);
+                chrome.storage.sync.set({'notes': updatedNotes}, function () {
+                    alert("New note added!");
+                })
+            })
+            location.reload()
+        }
+    }
 }, false);
 
 
@@ -44,3 +48,37 @@ let autoExpand = function (field) {
 
 	field.style.height = height + 'px';
 };
+
+function showNotes() {
+    chrome.storage.sync.get(['notes'], function (result) {
+        if (result.notes.length > 0) {
+            for (let note in result.notes) {
+                document.getElementById('notes').innerHTML += '<div class="link">' +
+                    '<button class="trash">' +
+                    '<i class="material-icons w3-large">delete</i></button>' + result.notes[note] + '</div>';
+            }
+        }
+    })
+
+    document.querySelector('.text').addEventListener('click', function (e) {
+        if (e.target && e.target.matches("i")) {
+            let delLink = e.target
+            let note = delLink.parentElement.parentElement.innerText.substring(6)
+            note = note.replace(/</g, '&lt;')
+              .replace(/>/g, '&gt;')
+            chrome.storage.sync.get(['notes'], function (result) {
+                const newList = result.notes;
+                for (var i = 0; i < newList.length; i++) {
+                    if (newList[i] == note) {
+                        newList.splice(i, 1);
+                    }
+                }
+                chrome.storage.sync.set({'notes': newList}, function () {
+                })
+            })
+        }
+        location.reload()
+    });
+}
+
+window.onload = function() {showNotes()};
